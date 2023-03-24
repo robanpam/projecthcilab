ee0683bb8b60ea2e6a659c1d34e95d6f02a12c0a
naulitaA <naulitarns@gmail.com>
2023-03-24 10:20:13 +0700

    characters page & about page
    design nya udah selesai yaa hehe

---
+++ about.js
@@ -0,0 +1,38 @@
+setTimeout(() => {
+    var modal = document.getElementById("photoModal");
+    var photobtn = document.getElementById("photoBtn");
+    var span = document.getElementsByClassName("close")[1];
+    var modal1 = document.getElementById("videoModal");
+    var videobtn = document.getElementById("videoBtn");
+
+    console.log(modal)
+    console.log(photobtn)
+    console.log(span)
+    console.log(modal1)
+    console.log(videobtn)
+    
+    photobtn.onclick = function(){
+        modal.style.display = "block";
+    }
+
+    videobtn.onclick = function(){
+        modal1.style.display = "block"
+    }
+    
+    span.onclick = function(){
+        modal.style.display = "none";
+    }
+
+    span.onclick = function(){
+        modal1.style.display = "none";
+    }
+    
+    window.onclick = function(event){
+        if(event.target == modal){
+            modal.style.display = "none";
+        }
+        if(event.target == modal1){
+            modal1.style.display = "none";
+        }
+    }
+}, 500);
