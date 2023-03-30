5c41ad7eaaccf7fe20d1135219c05565d66e93fd
robanpam <arikusumadewi3@gmail.com>
2023-03-30 10:23:16 +0700

    Update about page

--- about.js
+++ about.js
@@ -6,45 +6,66 @@ function showNavbar() {
         x.className = "menu";
     }
 }
+var modal = document.getElementById("photoModal");
+var photobtn = document.getElementById("photoBtn");
+var spans = document.querySelectorAll(".close");
+var modal1 = document.getElementById("videoModal");
+var videobtn = document.getElementById("videoBtn");
 
 setTimeout(() => {
-    var modal = document.getElementById("photoModal");
-    var photobtn = document.getElementById("photoBtn");
-    var span = document.getElementsByClassName("close")[1];
-    var modal1 = document.getElementById("videoModal");
-    var videobtn = document.getElementById("videoBtn");
-
     console.log(modal)
     console.log(photobtn)
-    console.log(span)
+    console.log(spans)
     console.log(modal1)
     console.log(videobtn)
-    
-    photobtn.onclick = function(){
+
+    photobtn.onclick = function () {
         modal.style.display = "block";
     }
 
-    videobtn.onclick = function(){
-        modal1.style.display = "block"
-    }
-    
-    span.onclick = function(){
-        modal.style.display = "none";
+    videobtn.onclick = function () {
+        modal1.style.display = "flex"
+        modal1.style.alignItems = "center"
     }
 
-    span.onclick = function(){
-        modal1.style.display = "none";
-    }
-    
-    window.onclick = function(event){
-        if(event.target == modal){
+    spans.forEach((span) => {
+        span.onclick = function () {
+            modal.style.display = "none";
+            modal1.style.display = "none";
+        }
+    })
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
             modal.style.display = "none";
         }
-        
     }
-    window.onclick = function(event){
-      if(event.target == modal1){
-          modal1.style.display = "none";
-      }
-  }
+
+    window.onclick = function (event) {
+        if (event.target == modal1) {
+            modal1.style.display = "none";
+        }
+    }
 }, 500);
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
